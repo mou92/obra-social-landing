e18e8aacc9ed0bf6b8fdae6acea61c868271a14d
mou92 <31455763+mou92@users.noreply.github.com>
2026-02-14 14:30:56 +0100

    Update empresasData.js

--- src/data/empresasData.js
+++ src/data/empresasData.js
@@ -3,4 +3,5 @@ export const empresas = [
   { nombre: "فحص بالاكوغرافي", logo: "https://placehold.co/150x80/1D3A75/FFFFFF?text=VitalCare" },
   { nombre: "تلقيحات وتحصينات دورية", logo: "https://placehold.co/150x80/198754/FFFFFF?text=MediTrust" },
   { nombre: "جراحة عامة", logo: "https://placehold.co/150x80/495057/FFFFFF?text=ObraMax" },
+  { nombre: "مستلزمات حيوانات", logo: "https://placehold.co/150x80/495057/FFFFFF?text=ObraMax" },
 ];
